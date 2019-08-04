7d5cfdd92f41ce123d4921bede548249cb7829fd
office-deskjet <office_deskjet@protonmail.com>
2019-08-03 22:06:50 -0700

    added pin definitions

--- FW/board_test.c
+++ FW/board_test.c
@@ -6,32 +6,32 @@
 #include <stdint.h>
 
 // pins to enable the RGB LEDs
-#define EN_RGB0 (1 << PA3) // pin 10
-#define EN_RGB1 (1 << PA4) // 11
-#define EN_RGB2 (1 << PA6) // 12
-#define EN_RGB3 (1 << PA7) // 13
-#define EN_RGB4 (1 << PA7) // 13
+// LEDS are common cathode, 0 enables, 1 disables
+#define EN_RGB0 (1 << PA3)
+#define EN_RGB1 (1 << PA4)
+#define EN_RGB2 (1 << PA5)
+#define EN_RGB3 (1 << PA6)
+#define EN_RGB4 (1 << PA7)
 
 // PWM output pins
-// footprint for the RBG LED has Green and Blue swapped, correcting in FW.
-#define R_PWM   (1 << PB1) // 8
-#define G_PWM   (1 << PB5) // 4
-#define B_PWM   (1 << PB3) // 6
+#define R_PWM   (1 << PB1)
+#define G_PWM   (1 << PB3)
+#define B_PWM   (1 << PB5)
 
-#define NUM_RGB_LEDs 4
+#define NUM_RGB_LEDs 5
 #define NUM_COLORS 3
 
-uint8_t en_rgb_led[NUM_RGB_LEDs] = {EN_RGB1, EN_RGB2, EN_RGB3, EN_RGB4};
+uint8_t en_rgb_led[NUM_RGB_LEDs] = {EN_RGB0, EN_RGB1, EN_RGB2, EN_RGB3, EN_RGB4};
 uint8_t colors[NUM_COLORS] = {R_PWM, G_PWM, B_PWM};
 
 void setup()
 {
     // enable output
-    DDRA = EN_RGB4 | EN_RGB3 | EN_RGB2 | EN_RGB1;
+    DDRA = EN_RGB4 | EN_RGB3 | EN_RGB2 | EN_RGB1 | EN_RGB0;
     DDRB = B_PWM | G_PWM | R_PWM;
 
     // diable all LEDs
-    PORTA &= ~EN_RGB4 & ~EN_RGB3 & ~EN_RGB2 & ~EN_RGB1;
+    PORTA &= EN_RGB4 & EN_RGB3 & EN_RGB2 & EN_RGB1;
 
     // enable all LEDS
     //PORTA |= EN_RGB4 | EN_RGB3 | EN_RGB2 | EN_RGB1;
